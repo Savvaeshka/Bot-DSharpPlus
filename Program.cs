9052a121ee9a017891c4a7f14f2649e74814cc5f
savva <savva@DESKTOP-VDOML2E>
2023-07-16 18:55:21 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using DSharpPlus;
+using System;
+namespace NewVersionBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var discrod = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "MTEyOTA2NzEyNTI2MTIxMzc0Nw.GJJ-6m.zqTjb_ItZm1U8qmAh4tyPIjeRv9cRgN9p79wdI",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+            });
+
+            discrod.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("/test"))
+                {
+                    await e.Message.RespondAsync("Pong");
+                }
+            };
+
+            await discrod.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
